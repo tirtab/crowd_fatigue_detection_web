163859d28d539bb550985c0edb2f48d662c350d8
muhammadRizki12 <m.rizkil7621@gmail.com>
2025-02-05 08:18:02 +0700

    add ecosystem configuration for face recognition app

--- face-recognition-master/face_recognition/arcface/weights/README.md
+++
@@ -1,3 +0,0 @@
-## Download Weights:
-
-- https://drive.google.com/drive/folders/1CHHb_7wbvfjKPFNKVBb76lL5sVfBLcv5?usp=sharing
---
+++ face-recognition/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: "app.py",
+    interpreter: "python",
+    watch: true,
+    windowsHide: true,
+    no_console: true
+  }]
+}
